1718181890bf2db0db21e4ab2f2d7eff997e5535
MrKami <mashhood.anwar@gmail.com>
2021-11-14 19:50:25 -0500

    added add function

--- tablepage.js
+++ tablepage.js
@@ -2,12 +2,23 @@ let table = document.getElementById("tabledata");
 let sheetValues = [];
 let currentValues, currentSheet;
 let tileName;
+
 let editWindow = document.getElementById("editwindow");
 let editRowWindow = document.getElementById("editrow");
+let addRowWindow = document.getElementById("addrowwindow");
+
 let date = document.getElementById("dateinput");
+let newDate = document.getElementById("newdateinput");
+
 let particular = document.getElementById("particularinput");
+let newParticular = document.getElementById("newparticularinput");
+
 let credit = document.getElementById("creditinput");
+let newCredit = document.getElementById("newcreditinput");
+
 let debit = document.getElementById("debitinput");
+let newDebit = document.getElementById("newdebitinput");
+
 let currentRow, rowId = 0;
 
 window.onload = function() {
@@ -91,41 +102,7 @@ function applyEdit() {
                 [currentRow.children[3].innerHTML],
                 [currentRow.children[4].innerHTML]], 
                 "A"+(rowId+1)+":E"+(rowId+1)
-                );
-
-
-    //let sheetId = -1;
-
-    // for (i = 0; i < sheets.length; i++) {
-    //     if (sheets[i].properties.title == tileName) {
-    //         sheetId = sheets[i].properties.sheetId;
-    //     }
-    // }
-
-    // let params = {
-    //     spreadsheetId: SPREADSHEET_ID,
-    //     range: tileName+"!A"+(rowId+1)+":"
-    // }
-    
-    // let body = {
-    //     requests: [{
-    //         deleteDimension: {
-    //             range: {
-    //                 sheetId: sheetId,
-    //                 dimension: "ROWS",
-    //                 startIndex: rowId,
-    //                 endIndex: rowId + 1 
-    //             }
-    //         }
-    //     }],
-    // }
-    
-    // let request = gapi.client.sheets.spreadsheets.values.update(params, body);
-    // request.then(function (response) {
-    //     console.log(response);
-    // }, function(reason) {
-    //     console.error(reason.result.error.message);
-    // });
+    );
 
     closeEditRow();
     closeEdit();
@@ -140,6 +117,10 @@ function editRow() {
     debit.setAttribute("value", currentRow.children[3].innerHTML);
     
     console.log(currentRow);
+}
+
+function addRow() {
+    addRowWindow.setAttribute("style", "display:block");
 }
 
 function deleteRow() {
@@ -192,10 +173,53 @@ function deleteRow() {
     }
 }
 
+function applyNewRow() {
+    if ((newDate.value != "" && newCredit.value != "") || (newDate.value != "" && newDebit.value != "")) {
+        console.log("made new row");
+        let newRow = table.insertRow(table.children.length-1);
+        
+        let cell0 = newRow.insertCell(0);
+        let cell1 = newRow.insertCell(1);
+        let cell2 = newRow.insertCell(2);
+        let cell3 = newRow.insertCell(3);
+        let cell4 = newRow.insertCell(4);
+
+        if (newCredit.value == "") newCredit.value = 0;
+        if (newDebit.value == "") newDebit.value = 0;
+
+        cell0.innerHTML = newDate.value;
+        cell1.innerHTML = newParticular.value;
+        cell2.innerHTML = newCredit.value;
+        cell3.innerHTML = newDebit.value;
+        cell4.innerHTML = (parseFloat(newCredit.value) - parseFloat(newDebit.value));
+
+        table.appendChild(newRow);
+
+        updateSheet(
+            tileName, 
+            [[cell0.innerHTML],
+            [cell1.innerHTML],
+            [cell2.innerHTML],
+            [cell3.innerHTML],
+            [cell4.innerHTML]], 
+            "A"+(table.children.length)+":E"+(table.children.length)
+        );
+
+        closeNewRow();
+    }
+    else if (newDate.value == "" || (newDate.value != "" && newCredit.value == "" ) || (newDate.value != "" && newDebit == "")) {
+        alert("Please input a new date and credit or debit value.");
+    }
+}
+
 function closeEdit() {
     editWindow.setAttribute("style", "display: none");
 }
 
 function closeEditRow() {
     editRowWindow.setAttribute("style", "display: none");
+}
+
+function closeNewRow() {
+    addRowWindow.setAttribute("style", "display:none");
 }
